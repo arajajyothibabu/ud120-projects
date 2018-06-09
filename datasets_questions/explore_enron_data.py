1c080bc2c12dfdfd7237008ae83639fc4fc59996
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-06-10 00:06:50 +0530

    exploring enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,37 @@
 """
 
 import pickle
+import fileinput
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print enron_data
+
+#persons
+print len(enron_data)
+
+#person properties
+print len(enron_data[enron_data.keys()[0]].keys())
+
+# How many POIs are there in the E+F dataset?
+pois = len([enron_data[person]["poi"] for person in enron_data.keys() if enron_data[person]["poi"] == True])
+print pois
+
+poi_names = open("../final_project/poi_names.txt", "r")
+
+print len(poi_names.readlines()[2:])
+
+print enron_data["PRENTICE JAMES"]['total_stock_value']
+
+print enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
+
+print enron_data["SKILLING JEFFREY K"]['exercised_stock_options']
+
+print len([enron_data[person]["salary"] for person in enron_data.keys() if enron_data[person]["salary"] != 'NaN'])
+
+print len([enron_data[person]["email_address"] for person in enron_data.keys() if enron_data[person]["email_address"] != 'NaN'])
+
+print len([enron_data[person]["total_payments"] for person in enron_data.keys() if enron_data[person]["total_payments"] == 'NaN']) * 100 / len(enron_data)
+
+print len([enron_data[person]["total_payments"] for person in enron_data.keys() if enron_data[person]["total_payments"] == 'NaN' and enron_data[person]["poi"] == True]) * 100 / pois
 
