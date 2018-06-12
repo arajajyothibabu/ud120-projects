c299849ce512f949b05afa0bb47fbb307ea974f5
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-06-12 10:57:40 +0530

    outlier detection

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,31 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL') #removing outlier
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+from operator import itemgetter
+
+#print data_dict
+l = []
+for key in data_dict.keys():
+    d = data_dict[key]
+    if d['bonus'] != 'NaN':
+        d['jyothi'] = key
+        l.append(d)
+
+print sorted(l, key=itemgetter('bonus'), reverse=True)
+
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,23 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    #print predictions, ages, net_worths
 
-    
-    return cleaned_data
+    ### your code goes here
+    for i in range(0, len(predictions)):
+        error = predictions[i][0] - net_worths[i][0]
+        #print ages[i], net_worths[i], error
+        cleaned_data.append((ages[i][0], net_worths[i][0], error * error))
+
+    #print cleaned_data
+    from operator import itemgetter
+    cleaned_data.sort(key=itemgetter(2), reverse=False)
+
+    #print cleaned_data
+
+    ninty_percent = int(0.9 * len(cleaned_data))
+    return cleaned_data[:ninty_percent]
 
