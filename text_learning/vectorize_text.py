45d48b0b6ac3d132e8ce5b0402393a48cb547685
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-06-15 10:40:42 +0530

    vectorizing text

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,27 +41,32 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        #temp_counter += 1
+        #if temp_counter < 200:
+        path = os.path.join('..', path[:-1])
+        #print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        stemmed = parseOutText(email)
+        ### use parseOutText to extract the text from the opened email
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        ### use str.replace() to remove any instances of the words
+        ### ["sara", "shackleton", "chris", "germani"]
+        replacebles = ["sara", "shackleton", "chris", "germani"]
+        for w in replacebles:
+            stemmed = stemmed.replace(w, '')
+        ### append the text to word_data
+        word_data.append(" ".join(stemmed.split())) #removing extra spaces before appending
+        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+        from_data.append(0 if name == "sara" else 1)
 
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
+        email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+
+#print word_data[152]
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
@@ -71,5 +76,15 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer(stop_words="english")
+
+vectorizer.fit_transform(word_data)
+
+unique_words = vectorizer.get_feature_names()
+
+print len(unique_words)
+
+print unique_words[34597]
 
